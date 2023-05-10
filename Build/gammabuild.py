8b004ad651d75aa6137b33eea3edc808e67d6eb5
pawanxcaliber <73450350+pawanxcaliber@users.noreply.github.com>
2023-05-10 07:05:29 +0100

    Update gammabuild.py

--- Build/gammabuild.py
+++ Build/gammabuild.py
@@ -20,13 +20,13 @@ conn.execute('''CREATE TABLE IF NOT EXISTS irrigation_data
 
 # Define a function to check moisture level and control the motor
 def check_moisture():
-    if GPIO.input(17) == 0: # Moisture level is low, turn on motor
-        GPIO.output(23, GPIO.HIGH) # Turn on relay
+    if GPIO.input(17) == 0: # Moisture level is high, turn off motor
+        GPIO.output(23, GPIO.HIGH) # Turn off relay
         motor_status = 'off'
         print("Moisture level is high. Motor is off.")
         send_sms('Motor off') # Send SMS notification
-    else: # Moisture level is high, turn off motor
-        GPIO.output(23, GPIO.LOW) # Turn off relay
+    else: # Moisture level is low, turn on motor
+        GPIO.output(23, GPIO.LOW) # Turn on relay
         motor_status = 'on'
         print("Moisture level is low. Motor is on.")
         send_sms('Motor on') # Send SMS notification
@@ -67,7 +67,7 @@ def get_last_n_readings(n):
 # Run the function in a loop
 while True:
     check_moisture()
-    time.sleep(100) # Wait for 5 seconds before checking again
+    time.sleep(1) # Wait for 5 seconds before checking again
 
 # Close the database connection
 conn.close()
